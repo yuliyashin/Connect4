841ef1e7de32fe2c71ca0aeb7caf24216c15c7b7
Julie <missjulieshin@gmail.com>
2018-12-07 02:32:29 -0800

    implemented sound and lotsa stuff

--- src/ConnectFourGame.java
+++ src/ConnectFourGame.java
@@ -6,26 +6,21 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
@@ -33,9 +28,9 @@ public class ConnectFourGame extends JFrame{
 	//dimensions
 	private int ROW = 6; //default 13 is max
 	private int COL = 7; //default	
-	private final int BLOCK_DIMENSIONS = 60; // image size (height and width)
-	private final int FRAME_WIDTH = COL * BLOCK_DIMENSIONS;
-	private final int FRAME_HEIGHT = (ROW * BLOCK_DIMENSIONS) + 100;
+	private final int BLOCK_DIMENSIONS = 59; // image size (height and width)
+	private int FRAME_WIDTH = COL * BLOCK_DIMENSIONS;
+	private int FRAME_HEIGHT = (ROW * BLOCK_DIMENSIONS) + 100;
 
 	//testing
 	Clip clip;
@@ -50,7 +45,7 @@ public class ConnectFourGame extends JFrame{
 	private static int P2Score = 0;
 	private static int AIScore = 0;
 
-	private JLabel[][] grid = new JLabel[ROW][COL];
+	private JLabel[][] grid;
 	private JLabel currentPlayer;
 	private JLabel player1;
 	private JLabel player2;
@@ -58,46 +53,60 @@ public class ConnectFourGame extends JFrame{
 	private JLabel player1Score;
 	private JLabel player2Score;
 	private JLabel playerAIScore;
+	private JLabel enterRow;
+	private JLabel enterCol;
+	private JLabel chooseGameMode;
 
 	private JPanel gridPanel;
 	private JPanel playerPanel;
 	private JPanel scorePanel;
-	private JPanel newGamePanel;
+	private JPanel bottomPanel;
 
 	//font 
-	private Font playerTurnFont = new Font("Courier", Font.ITALIC, 30);
-	private Font newGameFont = new Font("Courier", Font.BOLD, 15);
-	private Font font = new Font("Courier", Font.BOLD, 13);
+	private Font font = new Font("Courier", Font.PLAIN, FRAME_WIDTH/40);
 
 	// image icons
 	private ImageIcon emptyCircle = new ImageIcon(this.getClass().getResource("/emptyCircle.png"));
 	private ImageIcon yellowCircle = new ImageIcon(this.getClass().getResource("/happyYellowCircle.png"));
 	private ImageIcon redCircle = new ImageIcon(this.getClass().getResource("/madRedCircle.png"));
 	private ImageIcon greenCircle = new ImageIcon(this.getClass().getResource("/greenCircle.png"));
+	private ImageIcon gameBoard = new ImageIcon(this.getClass().getResource("/gameBoard.png"));
+	private ImageIcon versus = new ImageIcon(this.getClass().getResource("/versus.png"));
+	private ImageIcon volumeOn = new ImageIcon(this.getClass().getResource("/soundOn.png"));
+	private ImageIcon volumeOff = new ImageIcon(this.getClass().getResource("/soundOff.png"));
 	private ImageIcon PLAYER_ONE = yellowCircle;
 	private ImageIcon PLAYER_TWO = redCircle;
 	private ImageIcon PLAYER_AI = greenCircle; 
 	private ImageIcon activePlayer = PLAYER_ONE;
 
 	private JButton reset;
+	private JButton sound;
 
 	private Border winnerBorder = new LineBorder(Color.BLACK, 5);
 	private Border player1Border = new LineBorder(Color.YELLOW, 3);
 	private Border player2Border = new LineBorder(Color.RED, 3);
 	private Border playerAIBorder = new LineBorder(Color.GREEN, 3);
 
+	final String winningSound = "kids-saying-yay-sound-effect.wav";
+
+	boolean soundOn = true;
+
 	ConnectFourGame(){
 		setTitle("Connect 4!");
+		setIconImage(yellowCircle.getImage());
 		setLayout(new BorderLayout());
 		addMouseListener(new myMouseListener());
 
+		boardSizeOption();
 		gameMode = gameModeOption();
+		setDimensions();
 		System.out.print(gameMode);
 		if (gameMode == aiMode) {
 			createPvsAI();
-		}
-		else {
+		} else if (gameMode == playerMode) {
 			createPvsP();
+		} else { 
+			System.exit(0);
 		}
 
 		addComponents();
@@ -105,11 +114,30 @@ public class ConnectFourGame extends JFrame{
 		setVisible(true);
 	}
 
+	private void boardSizeOption() throws NumberFormatException {
+		enterRow = new JLabel("Enter the number of rows: ");
+		enterRow.setFont(font);
+		enterCol = new JLabel("Enter the number of columns: ");
+		enterCol.setFont(font);
+		String row = JOptionPane.showInputDialog(null, enterRow, "User input", JOptionPane.PLAIN_MESSAGE, gameBoard,  null, "").toString();
+		String col = JOptionPane.showInputDialog(null, enterCol,"User input", JOptionPane.PLAIN_MESSAGE, gameBoard,  null, "").toString();
+
+		ROW = Integer.parseInt(row);
+		COL = Integer.parseInt(col);
+	}
+
+	private void setDimensions() {
+		FRAME_WIDTH = COL * BLOCK_DIMENSIONS;
+		FRAME_HEIGHT = (ROW * BLOCK_DIMENSIONS) + 100;
+	}
+
 	// asks user which game mode they'd like to play before main game
 	private int gameModeOption() {
+		chooseGameMode = new JLabel("Player versus: ");
+		chooseGameMode.setFont(font);
 		String buttonArray[] = { "AI", "Player" };
-		int choice = JOptionPane.showOptionDialog(this, "Player versus:", "Select an Option",
-				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, buttonArray,
+		int choice = JOptionPane.showOptionDialog(this, chooseGameMode, "Select a game mode",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, versus, buttonArray,
 				buttonArray[0]);
 		return choice;
 	}
@@ -117,7 +145,7 @@ public class ConnectFourGame extends JFrame{
 	private void addComponents() {
 		add(playerPanel, BorderLayout.NORTH);
 		add(gridPanel, BorderLayout.CENTER);
-		add(newGamePanel, BorderLayout.SOUTH);
+		add(bottomPanel, BorderLayout.SOUTH);
 	}
 
 	private void setupFrame() {
@@ -332,7 +360,7 @@ public class ConnectFourGame extends JFrame{
 		playerPanel.setLayout(new FlowLayout());
 
 		currentPlayer = new JLabel("Player's turn. . .");
-		currentPlayer.setFont(playerTurnFont);
+		currentPlayer.setFont(font);
 		currentPlayer.setBorder(player1Border);
 
 		player1 = new JLabel("     Player:  " );
@@ -352,12 +380,17 @@ public class ConnectFourGame extends JFrame{
 
 		//		reset = new JButton("<html> New<br />Game</html>");
 		reset = new JButton("New Game");
-		reset.setFont(newGameFont);
+		reset.setFont(font);
 		reset.addActionListener(new ButtonHandler());
 
-		newGamePanel = new JPanel();
-		newGamePanel.setLayout(new FlowLayout());
-		newGamePanel.add(reset);	
+		sound = new JButton();
+		sound.addActionListener(new ButtonHandler());
+		sound.setIcon(volumeOn);
+
+		bottomPanel = new JPanel();
+		bottomPanel.setLayout(new FlowLayout());
+		bottomPanel.add(reset);	
+		bottomPanel.add(sound);
 	}
 
 	//	sets up game for Player versus Player
@@ -368,19 +401,20 @@ public class ConnectFourGame extends JFrame{
 		fillGrid();
 
 		playerPanel = new JPanel();
+
 		playerPanel.setLayout(new FlowLayout());
 
 		currentPlayer = new JLabel("Player 1's turn. . .");
-		currentPlayer.setFont(playerTurnFont);
+		currentPlayer.setFont(font);
 		currentPlayer.setBorder(player1Border);
 
-		player1 = new JLabel("      Player one:  " );
+		player1 = new JLabel("     Player one:  " );
 		player1.setFont(font);
-		player1Score = new JLabel("     " + String.valueOf(P1Score) + " ");
+		player1Score = new JLabel("  " + String.valueOf(P1Score) + " ");
 		player1Score.setFont(font);
-		player2 = new JLabel("      Player two: " );
+		player2 = new JLabel("     Player two: " );
 		player2.setFont(font);
-		player2Score = new JLabel("     " + String.valueOf(P2Score) + " ");
+		player2Score = new JLabel("  " + String.valueOf(P2Score) + " ");
 		player2Score.setFont(font);
 
 		playerPanel.add(currentPlayer);
@@ -389,17 +423,18 @@ public class ConnectFourGame extends JFrame{
 		playerPanel.add(player2);
 		playerPanel.add(player2Score);
 
-		newGamePanel = new JPanel();
-		newGamePanel.setLayout(new FlowLayout());
+		bottomPanel = new JPanel();
+		bottomPanel.setLayout(new FlowLayout());
 
 		reset = new JButton("New Game");
-		reset.setFont(newGameFont);
+		reset.setFont(font);
 		reset.addActionListener(new ButtonHandler());
 
-		newGamePanel.add(reset);
+		bottomPanel.add(reset);
 	}
 
 	private void fillGrid() {
+		grid = new JLabel[ROW][COL];
 		for (int j = 0; j < ROW; j++) {
 			for (int k = 0; k < COL; k++) {
 				grid[j][k] = new JLabel();
@@ -437,12 +472,17 @@ public class ConnectFourGame extends JFrame{
 							boolean playerWon = checkIfWon(i, col);
 
 							if (playerWon) {
+								if (soundOn) {
+									doPlay(winningSound);
+								}
 								P1Score++;
 								player1Score.setText(String.valueOf(P1Score));
 
 								int option = JOptionPane.showConfirmDialog(null, "Player 1 Wins! Would you like to play again?", "* We have a winner! *", JOptionPane.YES_NO_OPTION);
 								if (option == JOptionPane.YES_OPTION) {
 									resetBoard();
+								} else {
+									System.exit(0);
 								}
 							}
 
@@ -468,6 +508,9 @@ public class ConnectFourGame extends JFrame{
 							boolean playerWon = checkIfWon(i, col);
 
 							if (playerWon) {
+								if (soundOn) {
+									doPlay(winningSound);
+								}
 								P2Score++;
 								System.out.print("player2Score: " + P2Score);
 								player2Score.setText(String.valueOf(P2Score));
@@ -475,6 +518,8 @@ public class ConnectFourGame extends JFrame{
 								int option = JOptionPane.showConfirmDialog(null, "Player 2 Wins! Would you like to play again?", "* We have a winner *", JOptionPane.YES_NO_OPTION);
 								if (option == JOptionPane.YES_OPTION) {
 									resetBoard();
+								} else {
+									System.exit(0);
 								}
 							}
 
@@ -485,6 +530,8 @@ public class ConnectFourGame extends JFrame{
 
 								if (option == JOptionPane.YES_OPTION) {
 									resetBoard();
+								} else {
+									System.exit(0);
 								}
 							}
 
@@ -507,12 +554,17 @@ public class ConnectFourGame extends JFrame{
 							boolean playerWon = checkIfWon(i, col);
 
 							if (playerWon) {
+								if (soundOn) {
+									doPlay(winningSound);
+								}
 								P1Score++;
 								player1Score.setText(String.valueOf(P1Score));
 
 								int option = JOptionPane.showConfirmDialog(null, "Player 1 Wins! Would you like to play again?", "* We have a winner! *", JOptionPane.YES_NO_OPTION);
 								if (option == JOptionPane.YES_OPTION) {
 									resetBoard();
+								} else {
+									System.exit(0);
 								}
 							}
 
@@ -523,6 +575,8 @@ public class ConnectFourGame extends JFrame{
 
 								if (option == JOptionPane.YES_OPTION) {
 									resetBoard();
+								} else {
+									System.exit(0);
 								}
 							}
 
@@ -537,6 +591,9 @@ public class ConnectFourGame extends JFrame{
 											@Override
 											public void run() {
 												aiMove();
+												//if checkAIwin -> put move there
+												// else if opponentWin? -> block
+												// else random
 											}
 										}, 
 										randomTime 
@@ -602,11 +659,16 @@ public class ConnectFourGame extends JFrame{
 		boolean itsADraw = checkIfDraw();
 
 		if (playerWon) {
+			if (soundOn) {
+				doPlay(winningSound);
+			}
 			AIScore++;
 			playerAIScore.setText(String.valueOf(AIScore));
 			int option = JOptionPane.showConfirmDialog(null, "AI Wins! Would you like to play again?", "* We have a winner! *", JOptionPane.YES_NO_OPTION);
 			if (option == JOptionPane.YES_OPTION) {
 				resetBoard();
+			} else {
+				System.exit(0);
 			}
 		}
 
@@ -615,16 +677,14 @@ public class ConnectFourGame extends JFrame{
 
 			if (option == JOptionPane.YES_OPTION) {
 				resetBoard();
+			} else {
+				System.exit(0);
 			}
 		}
 
 		currentPlayer.setBorder(player1Border);
 		currentPlayer.setText("Player's turn. . .");
 		activePlayer = PLAYER_ONE;
-	}
-
-	private void playWinningSound(){
-
 	}
 
 	private void doPlay(final String url) {
@@ -640,7 +700,7 @@ public class ConnectFourGame extends JFrame{
 			System.err.println(e.getMessage());
 		}
 	}
-	
+
 	private void stopPlay(){
 		if (clip != null) {
 			clip.stop();
@@ -661,6 +721,15 @@ public class ConnectFourGame extends JFrame{
 
 				if (confirmed == JOptionPane.YES_OPTION) {
 					resetGame();
+				}
+			}
+			if (e.getSource() == sound) {
+				if (sound.getIcon() == volumeOn) {
+					sound.setIcon(volumeOff);
+					soundOn = false;
+				} else {
+					sound.setIcon(volumeOn);
+					soundOn = true;
 				}
 			}
 		}
